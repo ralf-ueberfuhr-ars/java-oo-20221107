43a56d511f0d4d9aded59934085d6cb1c531800b
Ralf Ueberfuhr <ralf.ueberfuhr@ars.de>
2022-11-08 14:17:33 +0100

    Mitarbeiter einstellen/entlassen

--- src/main/java/Firma.java
+++ src/main/java/Firma.java
@@ -3,16 +3,28 @@ public class Firma {
     String name;
     Ort ort;
     Konto konto;
+    // {null, null, null, null, ...., null}
     Mitarbeiter[] mitarbeiter = new Mitarbeiter[100];
 
 
     // einstellen
     void einstellen(Mitarbeiter ma) {
-
+        // TODO ist er schon drin?
+        for (int i=0; i< mitarbeiter.length; i++) {
+            if(mitarbeiter[i] == null) {
+                mitarbeiter[i] = ma;
+                break;
+            }
+        }
     }
     // entlassen
     void entlassen(Mitarbeiter ma) {
-
+        for (int i=0; i< mitarbeiter.length; i++) {
+            if(mitarbeiter[i] == ma) {
+                mitarbeiter[i] = null;
+                break;
+            }
+        }
     }
 
     // gehaltZahlen
